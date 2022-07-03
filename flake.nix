a4f9fbf1734c6b401868f248da0f591e9c8cb31b
Rob Glossop <robgssp@gmail.com>
2022-07-03 17:21:35 +0000

    Export a module and container config

--- flake.nix
+++ flake.nix
@@ -12,16 +12,53 @@
         pkgs = import nixpkgs { inherit system; };
         naersk-lib = pkgs.callPackage naersk { };
       in
-      {
-        defaultPackage = naersk-lib.buildPackage ./.;
+        {
+          defaultPackage = naersk-lib.buildPackage ./.;
 
-        defaultApp = utils.lib.mkApp {
-          drv = self.defaultPackage."${system}";
-        };
+          defaultApp = utils.lib.mkApp {
+            drv = self.defaultPackage."${system}";
+          };
 
-        devShell = with pkgs; mkShell {
-          buildInputs = [ cargo rustc rustfmt rls rustup pre-commit rustPackages.clippy ];
-          RUST_SRC_PATH = rustPlatform.rustLibSrc;
+          devShell = with pkgs; mkShell {
+            buildInputs = [ cargo rustc rustfmt rls rustup pre-commit rustPackages.clippy ];
+            RUST_SRC_PATH = rustPlatform.rustLibSrc;
+          };
+        }) //
+    {
+      nixosModule = { config, lib, pkgs, ... }:
+        with lib;
+        let cfg = config.services.counter-parser;
+            pkg = self.defaultPackage.x86_64-linux;
+        in {
+          options.services.counter-parser = {
+            enable = mkEnableOption "enables the counter-parser service";
+          };
+
+          config = mkIf cfg.enable {
+            systemd.services.counter-parser = {
+              wantedBy = [ "multi-user.target" ];
+              after = [ "network.target" ];
+              description = "Counter Parser service";
+              serviceConfig = {
+                Type = "simple";
+                Restart = "always";
+                RestartSec = "1";
+                ExecStart = "${pkg}/bin/web";
+              };
+            };
+          };
         };
-      });
+      nixosConfigurations.container = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [
+          self.nixosModule
+          ({ pkgs, ... }: {
+            boot.isContainer = true;
+            networking.firewall.allowedTCPPorts = [ 2369 ];
+            services.counter-parser.enable = true;
+            system.stateVersion = "22.05";
+          })
+        ];
+      };
+    };
 }
