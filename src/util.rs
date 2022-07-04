a26335b946b70f01f9eeab26441144f9df7801ae
Rob Glossop <robgssp@gmail.com>
2022-07-04 01:31:50 +0000

    Parse decimal numbers

--- src/util.rs
+++ src/util.rs
@@ -2,7 +2,7 @@ extern crate logos;
 use logos::Logos;
 use core::ops::Range;
 use regex::Regex;
-use crate::ast::Num;
+use crate::ast::{Num, to_num};
 use num::BigInt;
 
 #[derive(Logos, Debug, PartialEq, Clone)]
@@ -41,10 +41,10 @@ pub enum Token<'input> {
     RShift,
     #[regex(r"\d*(d|D)\d+", |lex| parse_roll(lex.slice()))]
     Roll((i64, i64)),
-    #[regex(r"-?[0-9]+", |lex| Num::from_integer(lex.slice().parse().unwrap()))]
-    #[regex(r"-?0x[0-9a-fA-F]+", |lex| parse_radix(lex.slice(), 16))]
-    #[regex(r"-?0o[0-7]+", |lex| parse_radix(lex.slice(), 8))]
-    #[regex(r"-?0b[01]+", |lex| parse_radix(lex.slice(), 2))]
+    #[regex(r"-?([0-9]+(\.[0-9]*)?|\.[0-9]+)", |lex| parse_decimal(lex.slice()))]
+    #[regex(r"-?0x([0-9a-fA-F]+(\.[0-9a-fA-F]*)?|\.[0-9a-fA-F]+)", |lex| parse_radix_prefixed(lex.slice(), 16))]
+    #[regex(r"-?0o([0-7]+(\.[0-7]*)?|\.[0-7]+)", |lex| parse_radix_prefixed(lex.slice(), 8))]
+    #[regex(r"-?0b([01]+(\.[01]*)?|\.[01]+)", |lex| parse_radix_prefixed(lex.slice(), 2))]
     Digits(Num),
     #[regex(r"[a-zA-Z][a-zA-Z0-9_]*", |lex| lex.slice())]
     Var(&'input str),
@@ -98,8 +98,43 @@ fn parse_roll(roll: &str) -> (i64, i64) {
     return (captures[1].parse().unwrap_or(1), captures[3].parse().unwrap());
 }
 
-fn parse_radix(istring: &str, radix: u32) -> Num {
-    BigInt::parse_bytes(&istring.as_bytes()[2..], radix).unwrap().into()
+fn parse_radix_prefixed(istring: &str, radix: u32) -> Num {
+    let negative = istring.chars().next().unwrap() == '-';
+    let start = if negative { 3 } else { 2 };
+    let sign = to_num(if negative { -1 } else { 1 });
+
+    sign * parse_radix(&istring.as_bytes()[start..], radix)
+}
+
+fn parse_decimal(istring: &str) -> Num {
+    let negative = istring.chars().next().unwrap() == '-';
+    let start = if negative { 1 } else { 0 };
+    let sign = to_num(if negative { -1 } else { 1 });
+
+    sign * parse_radix(&istring.as_bytes()[start..], 10)
+}
+fn parse_radix(istring: &[u8], radix: u32) -> Num {
+    lazy_static! {
+        static ref REGEX: Regex = Regex::new(r"^([a-zA-Z0-9]+)?(\.([a-zA-Z0-9]*))?$").unwrap();
+    }
+    println!("parse_radix of {:?}, radix {}", std::str::from_utf8(istring).unwrap(), radix);
+    let captures = REGEX.captures(std::str::from_utf8(istring).unwrap()).unwrap();
+
+    println!("Captures: {:?}", captures);
+
+    let int_part: Num = captures.get(1).map(
+        |mtch| BigInt::parse_bytes(&mtch.as_str().as_bytes(), radix).unwrap().into())
+        .unwrap_or(to_num(0));
+    let frac_part: Num = captures.get(3)
+        .filter(|mtch| mtch.start() != mtch.end())
+        .map(|mtch| {
+            let frac: Num = BigInt::parse_bytes(&mtch.as_str().as_bytes(), radix).unwrap().into();
+            let length = mtch.as_str().chars().count();
+            frac / (to_num(radix as i64).pow(length as i32))
+        })
+        .unwrap_or(to_num(0));
+
+    int_part + frac_part
 }
 
 // lalrpop takes an Iterator with item = Result<(Loc, Tok, Loc), LexError>
@@ -151,5 +186,19 @@ mod tests {
     #[should_panic]
     fn test_parse_roll_fails() {
         parse_roll("3d");
+    }
+
+    #[test]
+    fn test_parse_radix() {
+        assert_eq!(parse_decimal("12"), to_num(12));
+        assert_eq!(parse_decimal("1.5"), to_num(3) / to_num(2));
+        assert_eq!(parse_decimal(".05"), to_num(1) / to_num(20));
+        assert_eq!(parse_decimal("50."), to_num(50));
+        assert_eq!(parse_decimal("-50."), to_num(-50));
+        assert_eq!(parse_radix_prefixed("0x10.1", 16), to_num(257) / to_num(16));
+        assert_eq!(parse_radix_prefixed("-0x10.1", 16), to_num(-257) / to_num(16));
+        assert_eq!(parse_radix_prefixed("-0x.1", 16), to_num(-1) / to_num(16));
+        assert_eq!(parse_radix_prefixed("-0x1.", 16), to_num(-1));
+        assert_eq!(parse_radix_prefixed("0x1.", 16), to_num(1));
     }
 }
